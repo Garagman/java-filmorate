516f627d1e2f7ab14a529e595b098acfb9adc982
Ситников Сергей <sssitnikov@inbox.ru>
2026-04-16 20:17:09 +0300

    Выполнил задания 11 спринта

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,11 +1,12 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
-
+import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,10 +25,12 @@ public class FilmService {
     }
 
     public Film createFilm(Film film) {
+        validate(film);
         return filmStorage.create(film);
     }
 
     public Film updateFilm(Film film) {
+        validate(film);
         return filmStorage.update(film);
     }
 
@@ -39,14 +42,31 @@ public class FilmService {
         return filmStorage.getById(id);
     }
 
+    private void validate(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ValidationException("Название не может быть пустым");
+        }
+
+        if (film.getDescription() != null && film.getDescription().length() > 200) {
+            throw new ValidationException("Описание слишком длинное");
+        }
+
+        LocalDate minDate = LocalDate.of(1895, 12, 28);
+        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(minDate)) {
+            throw new ValidationException("Слишком ранняя дата релиза");
+        }
+
+        if (film.getDuration() == null || film.getDuration() <= 0) {
+            throw new ValidationException("Длительность должна быть положительной");
+        }
+    }
+
     public void addLike(Integer filmId, Integer userId) {
-        Film film = getById(filmId);
-        film.getLikes().add(userId);
+        getById(filmId).getLikes().add(userId);
     }
 
     public void removeLike(Integer filmId, Integer userId) {
-        Film film = getById(filmId);
-        film.getLikes().remove(userId);
+        getById(filmId).getLikes().remove(userId);
     }
 
     public List<Film> getPopular(int count) {
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -6,6 +6,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -24,10 +25,12 @@ public class UserService {
     }
 
     public User createUser(User user) {
+        validate(user);
         return userStorage.create(user);
     }
 
     public User updateUser(User user) {
+        validate(user);
         return userStorage.update(user);
     }
 
@@ -39,9 +42,32 @@ public class UserService {
         return userStorage.getById(id);
     }
 
+    private void validate(User user) {
+        if (user.getEmail() == null || !user.getEmail().contains("@")) {
+            throw new ValidationException("Некорректный email");
+        }
+
+        if (user.getLogin() == null || user.getLogin().isBlank()) {
+            throw new ValidationException("Логин не может быть пустым");
+        }
+
+        if (user.getLogin().contains(" ")) {
+            throw new ValidationException("Логин не должен содержать пробелы");
+        }
+
+        if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем");
+        }
+
+        // 🔥 ВАЖНО для теста
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+    }
+
     public void addFriend(Integer id, Integer friendId) {
         if (id.equals(friendId)) {
-            throw new ValidationException("Нельзя добавить самого себя в друзья");
+            throw new ValidationException("Нельзя добавить самого себя");
         }
 
         User user = getById(id);
@@ -60,18 +86,14 @@ public class UserService {
     }
 
     public List<User> getFriends(Integer id) {
-        User user = getById(id);
-        return user.getFriends().stream()
+        return getById(id).getFriends().stream()
                 .map(userStorage::getById)
                 .collect(Collectors.toList());
     }
 
     public List<User> getCommonFriends(Integer id, Integer otherId) {
-        User user = getById(id);
-        User otherUser = getById(otherId);
-
-        Set<Integer> friends = user.getFriends();
-        Set<Integer> otherFriends = otherUser.getFriends();
+        Set<Integer> friends = getById(id).getFriends();
+        Set<Integer> otherFriends = getById(otherId).getFriends();
 
         return friends.stream()
                 .filter(otherFriends::contains)

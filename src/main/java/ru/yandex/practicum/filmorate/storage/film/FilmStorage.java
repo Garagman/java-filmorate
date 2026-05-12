f2d09e3023f08d2cd79e928054077d0f55bbcda0
Ситников Сергей <sssitnikov@inbox.ru>
2026-05-13 00:32:20 +0300

    Выполнил задание 12 спринта

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,15 +1,22 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
 import ru.yandex.practicum.filmorate.model.Film;
+
 import java.util.List;
 
 public interface FilmStorage {
+
     Film create(Film film);
+
     Film update(Film film);
+
     Film getById(Integer id);
+
     List<Film> getAll();
-    
+
     List<Film> getPopular(int count);
+
     void addLike(Integer filmId, Integer userId);
+
     void removeLike(Integer filmId, Integer userId);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,16 +1,24 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
+
 import java.util.List;
 
 public interface UserStorage {
+
     User create(User user);
+
     User update(User user);
+
     User getById(Integer id);
+
     List<User> getAll();
 
     void addFriend(Integer userId, Integer friendId);
+
     void removeFriend(Integer userId, Integer friendId);
+
     List<User> getFriends(Integer userId);
+
     List<User> getCommonFriends(Integer userId, Integer otherId);
 }
